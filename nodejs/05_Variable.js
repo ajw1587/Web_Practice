ebe87b71a8112b670d64f001a548cbaaec4be24a
Inwoo <66319306+ajw1587@users.noreply.github.com>
2021-07-21 23:12:51 +0900

    Add files via upload

---
+++ nodejs/05_Variable.js
@@ -0,0 +1,15 @@
+var a = 1;
+console.log(a);
+a = 2;
+console.log(a);
+
+var letter = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit,\
+ sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. \
+ Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. \
+ Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. \
+ Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+console.log(letter);
+
+var node = 'Node.js';
+console.log('Practice' + node);
+var node2 = 'Node.js' + node;
